a5dcbf129b45a190653b78a5205779d47323b3ea
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-10-28 22:37:55 +0530

    update the class

--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,11 @@
 public class Manager extends CompanyEmployee {
+private String language;
 
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
 }
